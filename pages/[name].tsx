143641e56fe32afb9c1d605b52cb3d09d02941f8
jyp930 <jyp930@gmail.com>
2022-12-28 04:41:16 +0900

    4-4. getStaticPaths

---
+++ pages/[name].tsx
@@ -0,0 +1,26 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import type { Store } from '../types/store';
+
+interface Props {
+  store: Store;
+}
+
+const StoreDetail: NextPage<Props> = ({ store }) => {
+  return <div>name: {store.name}</div>;
+};
+export default StoreDetail;
+
+/** https://nextjs.org/docs/basic-features/data-fetching/get-static-paths */
+export const getStaticPaths: GetStaticPaths = async () => {
+  const stores = (await import('../public/stores.json')).default;
+  const paths = stores.map((store) => ({ params: { name: store.name } }));
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const stores = (await import('../public/stores.json')).default;
+  const store = stores.find((store) => store.name === params?.name);
+
+  return { props: { store } };
+};
