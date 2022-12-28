461fb91f8af351d3b2f397610ca66151a39a4d59
jyp930 <jyp930@gmail.com>
2022-12-28 12:28:43 +0900

    4-5. 매장 상세 페이지 UI

--- pages/[name].tsx
+++ pages/[name].tsx
@@ -1,12 +1,26 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import type { Store } from '../types/store';
+import styles from '../styles/detail.module.scss';
+import DetailHeader from '../components/home/DetailHeader';
+import DetailContent from '../components/home/DetailContent';
 
 interface Props {
   store: Store;
 }
 
 const StoreDetail: NextPage<Props> = ({ store }) => {
-  return <div>name: {store.name}</div>;
+  const expanded = true;
+
+  return (
+    <div className={`${styles.detailSection} ${styles.expanded}`}>
+      <DetailHeader
+        currentStore={store}
+        expanded={expanded}
+        onClickArrow={() => null}
+      />
+      <DetailContent currentStore={store} expanded={expanded} />
+    </div>
+  );
 };
 export default StoreDetail;
 
