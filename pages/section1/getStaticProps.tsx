42af7c10db4ba3980551eb70552bddff99289b46
chuhoon <khchu1220@naver.com>
2023-07-03 15:19:50 +0900

    Feat : getStaticProps 학습

--- pages/section1/getStaticProps.tsx
+++ pages/section1/getStaticProps.tsx
@@ -24,5 +24,7 @@ export async function getStaticProps() {
   return {
     props: { data },
     revalidate: 5 /** https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration */,
+    // 5초마다 서버가 request 받은지 5초가 지난 후 이 함수를 실행해서 데이터가 바뀌었으면 새로운 값으로 다시 pre-rendering 하는 것
+    // revalidate가 5로 지정되어 있지만 data가 변경되지 않으면 pre-rendering을 하지 않음.
   };
 }
