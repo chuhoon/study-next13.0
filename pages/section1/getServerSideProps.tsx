8fbe74fd30956f081605f315bd2ea9fe0e88f984
chuhoon <khchu1220@naver.com>
2023-07-03 18:32:29 +0900

    Study : getServerSideProps

--- pages/section1/getServerSideProps.tsx
+++ pages/section1/getServerSideProps.tsx
@@ -26,10 +26,22 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   //
   // In the background, a revalidation request will be made to populate the cache
   // with a fresh value. If you refresh the page, you will see the new value.
-  // res.setHeader(
-  //   'Cache-Control',
-  //   'public, s-maxage=5, stale-while-revalidate=10'
-  // );
+
+  // getStaticProps와 내용이 비슷함
+  // SSG와 달리 새로고침을 할 때마다 pending 상태가 되고 2초 후 rending 됨.
+  // SSR은 build time에 프리렌더링 되는 것이 아니고
+  // request time, 즉 페이지에 들어올 떄마다 프리렌더링 되는 것
+  // SSG에 비해 사용자 경험이 좋지 않고 반드시 request time마다 서버사이드에서 렌더링 해야되는 페이지에만 적용해야한다.
+  // 예로 사용자의 인증정보에 따라 변하는 페이지, 페이지가 동적으로 변해야하지만 보안이 중요한 페이지에 사용하는 것에 적합.
+  // getStaticProps와 같이 revalidate 하는 방법이 있는데 아래와 같은 방법
+
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=5, stale-while-revalidate=10'
+  );
+  // 2초간 pending 상태가 되고 5초 이내면 cache가 HIT로 캐시된 HTML을 보여주고, 5~10초 이내 하면 cache는 stale이고 뒤에서 새롭게 pre-rendering
+  // 10초 후에는 새롭게 렌더링 처음 진입했을 때와 같게 2초간 pending 상태가 되고 값이 변함
+  // 이것의 의미는 https://web.dev/i18n/ko/stale-while-revalidate/
 
   const delayInSeconds = 2;
   const data = await new Promise((resolve) =>
