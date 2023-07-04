d5faaa912fc0094c620c308f0480087aca13f0b3
chuhoon <khchu1220@naver.com>
2023-07-04 15:42:48 +0900

    Study : 헤더 컴포넌트 next style & image

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -6,9 +6,21 @@ interface Props {}
 
 const HeaderComponent = ({}: Props) => {
   return (
+    // scss를 설치하고 이런식으로 style을 가져올 수 있음.
     <header className={styles.header}>
       <div className={styles.flexItem}>
         <Link href="/" className={styles.box}>
+          {/* 1. public 폴더 바로 아래 
+              2. img link로 가져오기*/}
+          {/* 
+            1. next를 사용하라고 경고가 뜨고 있음 이유는?
+          */}
+          <img
+            src="https://lecture-1.vercel.app/inflearn.png"
+            width={110}
+            height={20}
+            alt="인프런 로고"
+          />
           <img src="/inflearn.png" width={110} height={20} alt="인프런 로고" />
         </Link>
       </div>
