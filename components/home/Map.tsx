ea63aec6656f0471d820be2ac8be0b8d09d01bc0
chuhoon <khchu1220@naver.com>
2023-07-05 10:19:05 +0900

    Study: next script option

--- components/home/Map.tsx
+++ components/home/Map.tsx
@@ -48,6 +48,15 @@ const Map = ({
 
   return (
     <>
+      {/* 
+          Next/Script
+          페이지가 로딩된 후 빠르게 보여지는거라면 afterInteractive,
+          조금 느려도 된다면 lazyOnload, afterInteractive보다 빠르게 가져오고 싶다면
+          beforeInteractive 사용, 하지만 beforeInteractive는 페이지 전체에서 사용할
+          스크립트에서만 사용해야함. Next의 권장은 pages/_document 파일
+          즉 모든 페이지에서 전역적으로 사용할 때만 beforeInteractive 사용을
+          권장하고 있음.
+      */}
       <Script
         strategy="afterInteractive"
         type="text/javascript"
